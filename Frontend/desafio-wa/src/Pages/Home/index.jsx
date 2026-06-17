a1fc9dfbd74d95dcdc582d8def75fcd7611e6f66
renezoka <rmmedeiros300@gmail.com>
2026-06-17 13:42:05 -0300

    criando o modelo html da aplicação

---
+++ Frontend/desafio-wa/src/Pages/Home/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './style.css'
+import Quit from '../../assets/X-27.png'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <form> 
+      <h1>Assistente Inteligente de Análise de Empresas</h1>
+      <input name="nome" type="text"/>
+      <button type="submit">Pesquisar</button>
+      </form>
+
+      <div>
+        <div>
+          <p>Nome da empresa: </p>
+          <p>Resumo da empresa: </p>
+          <p>Principais produtos e serviços: </p>
+          <p>Possíveis pontos fortes: </p>
+          <p>Possíveis riscos: </p>
+          <p>Sugestões de oportunidades de crescimento: </p>
+        </div>
+        <button img src={Quit} alt="Quit" className='quit'/>
+      </div>
+    </div>
+
+  )
+}
+
+export default Home
--- Frontend/desafio-wa/src/main.jsx
+++ Frontend/desafio-wa/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Home from './Pages/Home'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
